b6c556ac6d6c221e3e703ff2808a12fcf94751b9
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-11-20 21:45:09 -0500

    feat: improve the file generation and the denoup.toml parsing

--- src/toml_parse/init.ts
+++ src/toml_parse/init.ts
@@ -1,4 +1,5 @@
-import { askPrompt, path } from "src/deps.ts";
+import { path, tomlEncoding } from "src/deps.ts";
+import Ask from "https://deno.land/x/ask/mod.ts";
 import { currentDir, existsFile } from "utils/files.ts";
 import { error, info } from "utils/logs.ts";
 
@@ -15,45 +16,59 @@ end_of_line = crlf
 charset = utf-8
 `;
 
-const ask = new askPrompt.default();
+const ask = new Ask();
 
-export async function generateDenoUpProject() {
+// Start the terminal prompt
+
+const answers = await ask.prompt([
+  {
+    name: "projectName",
+    message: "Project Name ? ( NextChat ):",
+  },
+  {
+    name: "version",
+    message: "Version ? ( 1.0 ):",
+  },
+  {
+    name: "license",
+    message: "License ? ( MIT ):",
+  },
+  {
+    name: "author",
+    message: "Author? ( Teo Dev ):",
+  },
+]);
+
+const tomlData = {
+  info: {
+    name: answers.projectName,
+    version: answers.version,
+    license: answers.license,
+    author: answers.author,
+  },
+  deps: {
+    exampleDep: "https://deno.land/x/example/mod.ts",
+  },
+};
+
+export function generateDenoUpProject() {
   const DenoUpFile: boolean = existsFile(path.join(currentDir, "DenoUp.toml"));
   if (DenoUpFile == true) {
     error(
       "Exists the DenoUp file and the current project delete the file for create a new project",
     );
   } else {
+    Deno.writeTextFileSync(
+      path.join(currentDir, "DenoUp.toml"),
+      tomlEncoding.stringify(tomlData),
+    );
+    info("The <DenoUp.toml> file created");
     Deno.mkdir(path.join(currentDir, "src"));
-    info("The src folder created");
-    await Deno.writeTextFile(
+    info("The <src> folder created");
+    Deno.writeTextFileSync(
       path.join(currentDir, ".editorconfig"),
       editorConfigData,
     );
     info("The <.editorconfig> file created with the default config");
-
-    // Start the terminal prompt
-
-    const answers = await ask.prompt([
-      {
-        name: "projectName",
-        message: "Project Name ? ( NextChat ):",
-      },
-      {
-        name: "version",
-        message: "Version ? ( 1.0 ):",
-      },
-      {
-        name: "license",
-        message: "License ? ( MIT ):",
-      },
-      {
-        name: "generateProject",
-        type: "confirm",
-        message: "Create the DenoProject ? ( y, n )",
-      },
-    ]);
-
-    console.log(answers);
   }
 }
