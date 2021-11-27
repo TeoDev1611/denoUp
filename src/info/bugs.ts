3ed536eb2f5a32ff90dd0b2f7ffb84b597434953
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-11-27 11:30:00 -0500

    fix: change the message for open the browser

--- src/info/bugs.ts
+++ src/info/bugs.ts
@@ -2,6 +2,6 @@ import { openDenoBrowser } from "src/deps.ts";
 import { info } from "utils/logs.ts";
 
 export async function openRepoUrlBug() {
+  info("Opened the Repo Bug URL");
   await openDenoBrowser.open("https://github.com/TeoDev1611/denoUp/issues/new");
-  info("Opened the Repo Bug URL :p");
 }
